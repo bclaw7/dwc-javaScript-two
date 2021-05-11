1d8f4b76f0f8fc7ddca756db56855d2da9998d82
bclaw <bclaw8300@gmail.com>
2021-05-10 22:32:17 -0600

    added lesson one

---
+++ lesson-one/app.js
@@ -0,0 +1,13 @@
+import {convertToNumber} from 
+
+function submitFunction() {
+    let radius = document.getElementById("radius").value;
+    radius = parseFloat(radius);
+    let c = Math.PI * r * 2;
+    document.getElementById("output").innerHTML = c;
+}
+
+function submitAreaFunction() {
+    
+}
+
---
+++ lesson-one/validation.js
@@ -0,0 +1,9 @@
+export function convertToNumber(userString) {
+let num = parseFloat(userString);
+if(num) {
+    return num;
+} else {
+    return "Please enter a number."
+}
+}
+}
