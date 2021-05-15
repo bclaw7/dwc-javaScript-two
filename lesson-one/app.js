d2818766da829f2fa1e33dbf037eb984178891bb
bclaw <bclaw8300@gmail.com>
2021-05-15 08:46:10 -0600

    added display.js

--- lesson-one/app.js
+++ lesson-one/app.js
@@ -1,13 +1,27 @@
 import {calculateCircumference, calculateArea} from './circleCalc.js';
 import {convertToNumber} from './validation.js';
-
+import {} from './display.js'
+/*
 document.querySelector('#circumferenceButton').onclick = submitFunction;
 document.querySelector('#areaButton').onclick = submitAreaFunction;
+*/
+document.querySelector('#submitButton').addEventListener("click", function() {
+    submitFunction('circumference');
+}, false);
+document.querySelector('#submitAreaButton').addEventListener("click", function() {
+    submitFunction('area');
+}, false);
 
-function submitFunction() {
-    let userRadius = convertToNumber(document.getElementById("radiusCircumference").value);
-    let outputElement = document.getElementById('outputCircumference');
+
+function submitFunction(calculationType) {
+    
+    if(calculationType === 'circumference') {
+        let userRadius = convertToNumber(document.getElementById("area").value);
+        let outputElement = document.getElementById('outputCircumference');
+    }
+    
     if(userRadius) {
+        if(calculationType)
         let circumference = calculateCircumference(userRadius)
         outputElement.innerHTML = 'The circumference is ${circumference}.';
     } else {
@@ -15,14 +29,5 @@ function submitFunction() {
     }
 }
 
-    function submitAreaFunction() {
-        let UserRadius = convertToNumber(document.getElementById("radiusArea").value);
-        let outputElement = document.getElementById('outputArea');
-        if(UserRadius) {
-            let area = calculateArea(userRadius)
-            outputElement.innerHTML = 'The area is ${area}.';
-        } else {
-            outputElement.innerHTML = 'Please enter a valid number.'
-        }
-    }
+   
 
